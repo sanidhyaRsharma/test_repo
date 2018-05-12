d4e6335c1d492023c45e56fc67b85fefb993064f
sanidhyaRsharma <sanidhyarsharma@gmail.com>
2018-05-12 14:48:07 +0530

    commit c3

---
+++ counters.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	int count = 0, n= 20;
+	// Count number of odd numbers
+	for (int i = 0; i<n; i++){
+		if(i%2 != 0)
+			count ++;
+	}
+	cout<<count<<endl;
+	return 0;
+}
